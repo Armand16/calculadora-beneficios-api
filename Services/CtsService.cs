5c3aee520a59507b7072e59d95bf82b99994e37e
Armando Guevara <armandgv16@hotmail.com>
2024-11-13 14:56:43 -0500

    se agrego cts service y constants

---
+++ Services/CtsService.cs
@@ -0,0 +1,15 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace calculadora_beneficios.Services;
+
+public class CtsService
+{
+    public double Calcular(double sueldo, DateTime? fechaInicio, DateTime? fechaFin, bool asignacionFamiliar = false) {
+        var cts = 0d;
+
+        if (asignacionFamiliar)
+            sueldo += 
+
+        return 0;
+    }
+}
---
+++ Utils/Constants.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace calculadora_beneficios.Utils;
+
+public class Constants
+{
+    public const double SueldoMinimo = 1025;
+}
